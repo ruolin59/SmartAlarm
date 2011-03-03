d141360fbfd5066c1cdbeeba735ec2dec602d4e3
unknown <Hong@.(none)>
2011-03-02 17:33:17 -0800

    merge with GUI

---
+++ SmartAlarm/src/edu/ucla/cs/SmartAlarm/AlarmActivity.java
@@ -0,0 +1,13 @@
+package edu.ucla.cs.SmartAlarm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AlarmActivity extends Activity{
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.time_pick);
+	}
+
+}
--- SmartAlarm/src/edu/ucla/cs/SmartAlarm/ServiceStarter.java
+++ SmartAlarm/src/edu/ucla/cs/SmartAlarm/ServiceStarter.java
@@ -1,6 +1,7 @@
 package edu.ucla.cs.SmartAlarm;
 
 import android.app.Activity;
+import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,14 +9,46 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TabHost;
 
-public class ServiceStarter extends Activity{
+public class ServiceStarter extends TabActivity{
 	Button buttonStart, buttonStop;
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.servicestarter);
-    }
+    //@Override
+	//public void onCreate(Bundle savedInstanceState) {
+    //  super.onCreate(savedInstanceState);
+    //  setContentView(R.layout.servicestarter);
+    //}
+    
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tabhost);
+		
+	    TabHost tabhost = getTabHost();  // The activity TabHost
+	    TabHost.TabSpec spec;  // Reusable TabSpec for each tab
+	    Intent intent;  // Reusable Intent for each
+	    
+	    // Create an Intent to launch an Activity for the tab (to be reused)
+	    intent = new Intent().setClass(this, AlarmActivity.class);
+	    
+	    // Initialize a TabSpec for each tab and add it to the TabHost
+	    spec = tabhost.newTabSpec("Home");
+	    spec.setContent(intent);
+	    spec.setIndicator("Home");
+	    tabhost.addTab(spec);
+	    
+	    // Create an Intent to launch an Activity for the tab (to be reused)
+	    intent = new Intent().setClass(this, SettingsActivity.class);
+	    
+	    // Initialize a TabSpec for each tab and add it to the TabHost
+	    spec = tabhost.newTabSpec("Settings");
+	    spec.setContent(intent);
+	    spec.setIndicator("Settings");
+	    tabhost.addTab(spec);
+	    
+	    tabhost.setCurrentTab(0);
+
+	}
     
 	public void SASSH(View view) {
 		switch (view.getId()) {
