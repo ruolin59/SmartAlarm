120289539532a3e3d5fd6850e995a29cfafc1bdb
unknown <Hong@.(none)>
2011-03-02 17:39:30 -0800

    retry to merge

--- SmartAlarm/src/edu/ucla/cs/SmartAlarm/settingsActivity.java
+++ SmartAlarm/src/edu/ucla/cs/SmartAlarm/settingsActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class settingsActivity extends Activity{
+public class SettingsActivity extends Activity{
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
